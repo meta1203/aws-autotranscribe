9060dea52d74c338fec548673e8114f3238ed422
Hunter Hancock <hancock.hunter@gmail.com>
2022-09-01 14:30:07 -0600

    update input.js to provide additional input values

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Autotranscribe #
 
-*Instructions*
+**Instructions**
 
 To install, run `terraform apply`. This will generate an S3 bucket in the format `autotranscribe-[random string]`
 with two lambda functions.
 
-Simply upload any valid file to the folder/prefix `input` in this bucket to kick off a transcription job.
+Simply upload any valid file to the folder/prefix `input/` in this bucket to kick off a transcription job.
 
 To select the number of speakers in the audio, add `[#]people` to the filename.
 
-Amazon Transcribe json files will be saved to `output`, while the compiled transcriptions will saved to `transcriptions`.
+Amazon Transcribe json files will be saved to `output/`, while the compiled transcriptions will saved to `transcriptions/`.
 
-*Warning*
+**Warning**
 
 The terraform script will progressively move objects over 20 megabytes in the bucket from Standard to Infrequent Access
 to Deep Archive by default. To disable this behavior entirely, delete the `lifecycle.tf` file before executing terraform.
--- src/input.js
+++ src/input.js
@@ -2,45 +2,71 @@ const AWS = require('aws-sdk');
 const transcribe = new AWS.TranscribeService();
 
 exports.handler =  async function(event, context) {
+  let values = [];
+  // S3 event
   if (event.Records) {
     for (let record of event.Records) {
-      const s3 = record.s3;
       console.log(JSON.stringify(s3, null, 2));
-      const bucket = s3.bucket.name;
-      const key = s3.object.key;
+      
+      values.push({
+	bucket: record.s3.bucket.name,
+	key: record.s3.object.key,
+      });
+    }
+  } else if (event.bucket && event.key) {
+    values.push({
+      bucket: event.bucket,
+      key: event.key,
+    });
+  } else if (event.uri) {
+    values.push({
+      // get bucket value from URI
+      bucket: event.uri.match(/(?<=s3:\/\/)(.+?)(?=\/)/g)[0],
+      // get key value from URI
+      key: event.uri.match(/(?<=s3:\/\/.+?\/).*/g)[0],
+    });
+  } else if (event.objects) {
+    for (let obj of event.objects) {
+      values.push({
+	bucket: obj.bucket,
+	key: obj.key,
+      });
+    }
+  }
 
-      if (key.toLowerCase().endsWith(".amr") || key.toLowerCase().endsWith(".flac") ||
-	  key.toLowerCase().endsWith(".mp3") || key.toLowerCase().endsWith(".mp4") ||
-	  key.toLowerCase().endsWith(".ogg") || key.toLowerCase().endsWith(".webm") ||
-	  key.toLowerCase().endsWith(".wav")) {
+  for (let obj of values) {
+    const key = obj.key
+    const bucket = obj.bucket;
+    
+    if (key.toLowerCase().endsWith(".amr") || key.toLowerCase().endsWith(".flac") ||
+	key.toLowerCase().endsWith(".mp3") || key.toLowerCase().endsWith(".mp4") ||
+	key.toLowerCase().endsWith(".ogg") || key.toLowerCase().endsWith(".webm") ||
+	key.toLowerCase().endsWith(".wav")) {
 
-	// check for "##people" in the file name and set the person scanner to that number
-	// default to 10 people if not found
-	const matched = key.match(/(\d{1,2})(?=people)/);
-	const numPeople = (matched && matched[0] && parseInt(matched[0])) || 10;
+      let config = {
+	Media: {
+	  MediaFileUri: "s3://" + bucket + "/" + key
+	},
+	TranscriptionJobName: "autotranscribe-" + name,
+	LanguageCode: "en-US",
+	OutputBucketName: bucket,
+	OutputKey: key.replace(/input/g, "output").replace(/[^a-zA-Z0-9-_.!*'()/]/g, '') + ".json",
+      };
 
-	const name = key.split("/").pop().replace(/[^0-9a-zA-Z._-]/g, '');
-	
-	await transcribe.startTranscriptionJob({
-	  Media: {
-	    MediaFileUri: "s3://" + bucket + "/" + key
-	  },
-	  TranscriptionJobName: "autotranscribe-" + name,
-	  LanguageCode: "en-US",
-	  OutputBucketName: bucket,
-	  OutputKey: key.replace(/input/g, "output").replace(/[^a-zA-Z0-9-_.!*'()/]/g, '') + ".json",
-	  /* JobExecutionSettings: {
-	    AllowDeferredExecution: true,
-	    DataAccessRoleArn: process.env.TRANSCRIBE_ROLE
-	  }, */
-	  Settings: {
-	    ShowSpeakerLabels: true,
-	    MaxSpeakerLabels: numPeople
-	  }
-	}).promise();
-      } else {
-	console.log("Invalid filetype: " + key.split(".").pop());
-      }
+      // check for "##people" in the file name and set the person scanner to that number
+      // default to single speaker if not found
+      const matched = key.match(/(\d{1,2})(?=people)/);
+      const numPeople = (matched && matched[0] && parseInt(matched[0])) || 0;
+      if (numPeople) config.Settings = {
+	ShowSpeakerLabels: true,
+	MaxSpeakerLabels: numPeople,
+      };
+
+      const name = key.split("/").pop().replace(/[^0-9a-zA-Z._-]/g, '');
+      
+      await transcribe.startTranscriptionJob(config).promise();
+    } else {
+      console.log("Invalid filetype: " + key.split(".").pop());
     }
   }
 };
